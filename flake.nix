14de3738a5e45a879f4c788cbe065ad4799b6100
GekOrdep <gekordep@gmail.com>
2024-10-30 13:42:43 -0300

    adicionado config no git

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "O conglomerado dos meus sistemas";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs-stable, nixpkgs, home-manager, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in
+  {
+    nixosConfigurations = {
+      rock = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./systems/rock
+        ];
+        specialArgs = {
+          inherit inputs;
+        };
+      };
+    };
+
+    homeConfigurations = {
+      gekordep = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./users/gekordep
+        ];
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+      };
+    };
+  };
+}
